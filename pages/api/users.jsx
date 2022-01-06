705af3206feb3a851dab7303e39e6e11e6b7db2b
Your NameFalconi03 <falconi_fal@hotmail.com>
2022-01-06 10:19:44 -0300

    front end ok

--- pages/api/users.jsx
+++ pages/api/users.jsx
@@ -1,18 +1,26 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import Login from "../../components/Login";
 import connect from "../../utils/Database.ts";
 
+const { ObjectId } = require("mongodb");
 
 
 export default async function user(req,res) {
+    req.method = 'GET'
     if (req.method === 'POST'){
         const {db} = await connect();
-        const {name} = req.body
-        const response = await db.collection('users').insertOne({
-        name : 'Anderson',
-        age : 31
-        });
+        const data = req.body
+        const response = await db.collection('users').insertOne(data);
     
-        res.status(200).json({name})
+        res.status(200).send(response)
+    }if (req.method === 'GET'){
+        const {db} = await connect()
+        const data = "61d2f483cad3b76b8daa6235"
+        const response = await db
+            .collection('users')
+            .find({_id: ObjectId(data)})
+            .toArray();
+        res.status(200).send(response[0])
+        console.log(response[0])
     }else{
         
         res.status(400).json('Wrong method')
